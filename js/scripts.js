09c18753c72a38490a27d8acf8b022210924e4c4
Ashlin Aronin & Micah Courey <student@epicodus.com>
2015-09-15 14:47:04 -0700

    Implement spec for movies after 8:00pm

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,11 @@ Ticket.prototype.calculatePrice = function() {
         // Calculate any discounts for adults based on ticket time
         if (this.timeHours < 18) {
             return 4;
+        // Either the hours hand is past 8, or we are somewhere between 8 and 9
+    } else if ((this.timeHours > 20) || (this.timeHours === 20 && this.timeMinutes > 0)) {
+            return 7;
         } else {
             return 5;
         }
-
-
     }
 };
